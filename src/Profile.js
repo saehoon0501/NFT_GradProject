17032cbd5ee5d9059813afb69f850783c44e1acc
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2022-07-21 15:32:23 +0900

    프로필 업데이트

--- src/Profile.js
+++ src/Profile.js
@@ -1,6 +1,6 @@
 import "./Profile.css"
 import img from "./images/user.png";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const ProfilePic = () => {
     const [pic, setPic] = useState(img);
@@ -12,9 +12,10 @@ const ProfilePic = () => {
     };
 
     return(
-        <div className="profile-info">
+        <div className="profile-info clickable-img">
             <input type="file" style={{display:"none"}} ref={inputFile} onChange={(event)=>{setPic(URL.createObjectURL(event.target.files[0]))}} />
             <img src={pic} onClick={handlePic} style={{width:"450px", borderRadius:"20px"}}/>
+            <p className="description">이미지 수정</p>
         </div>
     );
 }
@@ -22,37 +23,63 @@ const ProfilePic = () => {
 const ProfileCaption = () => {
     
     const [caption, setCaption] = useState("안녕하세훈");
-    const [ProfileName, setProfileName] = useState("");
+    const [profileName, setProfileName] = useState("0xbe38d61731FB86D9A981f38F1bD73b106E80ce32");
     const [edit, setEdit] = useState(true);
 
-    return(
-        <div className="profile-info profile-about" style={{ width:"400px",padding:"1% 5%"}}>
-            <div style={{display:"flex"}}>
-                {edit?
-                <h2>byun_0501</h2>
-                :<input type={"text"}>
+    const nameRef = useRef(profileName);
+    const captionRef = useRef(caption);
 
-                </input>
-                }  
-                <input style={{fontSize:"16px", padding:"0 16px", margin:"auto", backgroundColor:"white",
-                    border:"0", borderRadius:"2pt", 
-                    boxShadow:"0 0 0 1px lightgray"}} onClick={edit?()=>{setEdit(false)}:()=>{setEdit(true)}} type="button" value="프로필 편집"/>
-            </div>
-            <div className="profile-text">
-                <h3>게시물 3</h3>
-                <h3 style={{color:"blue"}}>RGB 100</h3>
-            </div>
-            <div>
+    const update = () => {
+        nameRef.current = profileName;
+        captionRef.current = caption;
+        setEdit(true);
+    }
+
+    const updateNot = () => {
+        setEdit(true);
+        setProfileName(nameRef.current);
+        setCaption(captionRef.current);
+    }
+
+    return(
+        <div className="profile-info profile-about" style={{ width:"400px", padding:"1% 5%"}}>
+            <div style={{width:"100%", fontSize:"1.3em"}}>
+                {edit?
+                <div style={{display:"flex", width:"100%"}}>
+                    <div style={{wordWrap:"break-word", width:"100%", marginRight:"auto"}}>
+                        <h3>{nameRef.current}</h3>
+                    </div>
+                    <input className="clickable-input" onClick={()=>{ setEdit(false)}} type="button" value="프로필 편집"/>
+                </div>
+                :<div style={{display:"block", width:"100%"}}>
+                    사용자 이름
+                    <textarea onChange={(event)=>{setProfileName(event.target.value)}} value={profileName}
+                        style={{width:"100%", wordWrap:"break-word", 
+                        fontSize:"1.1em", fontWeight:"bolder",resize:"none", padding:"2%"}}/>
+                </div>
+                }
                 
+                <div className="profile-text">
+                    <h3>게시물 3</h3>
+                    <h3 style={{color:"blue"}}>RGB 100</h3>
+                </div>
             </div>
-            <div>
+                
+            <div style={{fontSize:"1.3em"}}>
             {edit?
                 <span>
-                   {caption}
+                   {captionRef.current}
                 </span>
-            :<textarea>
-
-            </textarea>
+            :<div>
+            소개
+            <textarea onChange={(event)=>{setCaption(event.target.value)}} value={caption}
+                style={{width:"100%", height:"150px", resize:"none", fontSize:"1em"}}/>
+            <div style={{display:"flex", margin:"auto", justifyContent:"center", margin:"5% auto"}}>
+                    <input className=" clickable-input"  
+                        onClick={update} type="button" value="확인"/>
+                    <input className=" clickable-input" onClick={updateNot} type="button" value="취소"/>
+                </div>
+            </div>
             }
             </div>
         </div>
