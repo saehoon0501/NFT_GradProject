101735fbad7ced04ca5e5d0183b6132efe65ab3d
Billy <tnehd1998@naver.com>
2022-09-02 16:32:53 +0900

    style: Header 반응형으로 수정

--- Frontend/src/components/common/Header.js
+++ Frontend/src/components/common/Header.js
@@ -1,52 +1,15 @@
 import { useNavigate } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import { useQuery } from "react-query";
-import axios from "axios";
 
-import "../../App.css";
-import { Modal, showModal } from "./Modal";
-import { IconButton } from "@mui/material";
+import "./Header.css";
 import { getUser } from "../../api/UserApi";
 import { isLoginState } from "../../store";
-
-const baseURL = "http://localhost:4000";
-const token = window.localStorage.getItem("NFTLogin");
 
 export const Header = (props) => {
   const [isAuth, setIsAuth] = useRecoilState(isLoginState);
   const navigate = useNavigate();
-
-  const createPost = async ({ username, caption, image }) => {
-    const data = new FormData();
-
-    data.append("file", image);
-    data.append("caption", caption);
-    data.append("user", username);
-
-    await axios
-      .post(`${baseURL}/api/post`, data, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then((res) => {
-        console.log(res.data);
-      });
-  };
-
-  const handleClick = () => {
-    showModal();
-  };
-
-  const {
-    isError,
-    isLoading,
-    error,
-    data: user,
-  } = useQuery("user", ({ signal }) => getUser(signal));
-
-  console.log(user);
+  const { data: user } = useQuery("user", ({ signal }) => getUser(signal));
 
   const showProfile = () => {
     navigate("/profile");
@@ -59,40 +22,18 @@ export const Header = (props) => {
   return (
     <>
       {!isAuth && (
-        <div className="app__header">
-          <div className="app__headerWrapper">
-            <img
-              className="clickable"
-              src=" https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
-              alt="Instagram original logo"
-              onClick={showSns}
-            />
-            <div className="app__headerButtons">
-              {/* <input
-                type="image"
-                src={add}
-                alt="add Button"
-                onClick={handleClick}
-                className="addButton"
-              /> */}
-              <Modal username={user?.profile.username} newPosts={createPost} />
-              <IconButton
-                size="small"
-                onClick={showProfile}
-                style={{ marginLeft: "50px", backgroundColor: "transparent" }}
-              >
-                <img
-                  src={user?.profile.profile_pic}
-                  alt="profile picture"
-                  style={{
-                    width: "40px",
-                    height: "40px",
-                    borderRadius: "10px",
-                  }}
-                />
-              </IconButton>
-            </div>
-          </div>
+        <div className="wrapper">
+          <img
+            src=" https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+            alt="Instagram original logo"
+            onClick={showSns}
+          />
+          <img
+            onClick={showProfile}
+            className="profile_img"
+            src={user?.profile.profile_pic}
+            alt="profile picture"
+          />
         </div>
       )}
     </>
