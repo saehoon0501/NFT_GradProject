5280b8d17d51a96d6062dca805a3277bce14295e
Billy <tnehd1998@naver.com>
2022-09-04 21:02:25 +0900

    style: 프로필 이미지 변경 팝업 반응형으로 수정

--- Frontend/src/components/profile/ProfileChangeImage.jsx
+++ Frontend/src/components/profile/ProfileChangeImage.jsx
@@ -23,20 +23,16 @@ export const ProfileChangeImage = ({ user }) => {
     profilePicMutate.mutate(profile_pic);
   };
 
-  const ownedNFTs = user.ownerOfNFT.map((NFTs) => {
-    return NFTs.NFT_URL.map((NFT) => {
-      return (
-        <div className="profile-info clickable-img" style={{ margin: "0 3%" }}>
-          <img
-            src={`${NFT}`}
-            onClick={(event) => updatePic(event)}
-            alt={NFTs.collection_id}
-            style={{ width: "250px", borderRadius: "20px" }}
-          />
-        </div>
-      );
-    });
-  });
+  const ownedNFTs = user.ownerOfNFT[0].NFT_URL.map((NFT, index) => (
+    <div className="profile-info clickable-img" style={{ margin: "0 3%" }}>
+      <img
+        src={`${NFT}`}
+        onClick={(event) => updatePic(event)}
+        alt={index}
+        style={{ width: "250px", borderRadius: "20px" }}
+      />
+    </div>
+  ));
 
   const handleClose = () => {
     setChangeImage(false);
@@ -51,7 +47,21 @@ export const ProfileChangeImage = ({ user }) => {
           </span>
           <h3 className="modal-title">프로필 사진 NFT 선택 </h3>
         </div>
-        <div className="modal-nfts-imgs">{ownedNFTs}</div>
+        <div className="modal-nfts-imgs">
+          {user.ownerOfNFT[0].NFT_URL.map((NFT, index) => (
+            <div
+              className="profile-info clickable-img"
+              style={{ margin: "0 3%" }}
+            >
+              <img
+                className="modal-nft-img"
+                src={`${NFT}`}
+                onClick={(event) => updatePic(event)}
+                alt={index}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
