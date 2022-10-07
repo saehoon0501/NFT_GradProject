31c9d15338e84c98221bfc9aa35f53cde4ea2d13
Billy <tnehd1998@naver.com>
2022-10-07 19:04:39 +0900

    feat: 투표 생성 팝업 제작

--- Frontend/src/components/main/Vote.jsx
+++ Frontend/src/components/main/Vote.jsx
@@ -5,6 +5,8 @@ export const Vote = ({ data, currentVoteContent, setCurrentVoteContent }) => {
   const [showPopUp, setShowPopUp] = useState(false);
   const [selectedOption, setSelectedOption] = useState("");
   const [createVote, setCreateVote] = useState(false);
+  const [voteTitle, setVoteTitle] = useState("");
+  const [voteOptions, setVoteOptions] = useState("");
 
   const onClickVoteSubject = (voteData) => {
     setShowPopUp(true);
@@ -32,6 +34,24 @@ export const Vote = ({ data, currentVoteContent, setCurrentVoteContent }) => {
 
   const onClickCloseCreateVotePopUp = () => {
     setCreateVote(false);
+  };
+
+  const onChangeInput = (event) => {
+    const { value } = event.target;
+    setVoteTitle(value);
+  };
+
+  const onChangeTextarea = (event) => {
+    const { value } = event.target;
+    setVoteOptions(value);
+  };
+
+  const onClickCreateVoteSubmit = () => {
+    console.log("제목", voteTitle);
+    console.log("옵션", voteOptions.split(","));
+    setCreateVote(false);
+    setVoteTitle("");
+    setVoteOptions("");
   };
 
   return (
@@ -81,14 +101,39 @@ export const Vote = ({ data, currentVoteContent, setCurrentVoteContent }) => {
         </div>
       )}
       {createVote && (
-        <div className="vote_popup">
+        <div className="create_vote_popup">
           <button
             onClick={onClickCloseCreateVotePopUp}
             className="vote_popup_closeBtn"
           >
             ✕
           </button>
-          <h1>Create Vote</h1>
+          <h1 className="create_vote_popup_title">투표 항목 생성</h1>
+          <div className="create_vote_popup_content">
+            <p className="create_vote_popup_content_title">투표 제목</p>
+            <input
+              className="create_vote_popup_content_input"
+              type="text"
+              value={voteTitle}
+              onChange={onChangeInput}
+            />
+          </div>
+          <p>각 항목 사이에 쉼표를 포함하여 작성해주세요.</p>
+          <p>Ex) 메시,호날두,홀란드</p>
+          <div className="create_vote_popup_content">
+            <p className="create_vote_popup_content_title">선택 항목</p>
+            <textarea
+              className="create_vote_popup_content_textarea"
+              value={voteOptions}
+              onChange={onChangeTextarea}
+            />
+          </div>
+          <button
+            onClick={onClickCreateVoteSubmit}
+            className="vote_popup_submitBtn create_vote_popup_btn"
+          >
+            제작하기
+          </button>
         </div>
       )}
     </div>
