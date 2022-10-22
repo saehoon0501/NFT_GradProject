51e7640e8bcbc45040c9f4560a00c3576df6c39f
Billy <tnehd1998@naver.com>
2022-10-22 21:09:11 +0900

    feat: 검색 창 Enter 누를 시 자동 검색

--- Frontend/src/components/common/Header.js
+++ Frontend/src/components/common/Header.js
@@ -73,6 +73,12 @@ export const Header = ({ socketValue }) => {
     setShowAlarm(!showAlarm);
   };
 
+  const handleEnter = () => {
+    if (window.event.keyCode == 13) {
+      onClickSearch();
+    }
+  };
+
   const onClickSearch = () => {
     if (keyword.length < 2) {
       alert("검색어는 2글자 이상이어야 합니다.");
@@ -107,7 +113,12 @@ export const Header = ({ socketValue }) => {
             onClick={showSns}
           />
           <div className="header_searchBar_wrapper">
-            <input value={keyword} type="text" onChange={onChangeSearch} />
+            <input
+              value={keyword}
+              type="text"
+              onChange={onChangeSearch}
+              onKeyUp={handleEnter}
+            />
             <button onClick={onClickSearch}>🔍</button>
           </div>
           <div className="header_menus">
