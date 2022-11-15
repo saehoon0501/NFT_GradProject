29ac0de7cce8e9a1dd86df84e1488534985c0705
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2022-11-15 11:18:09 +0900

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -75,9 +75,9 @@ namespace.on('connection', (socket)=>{
     })
 })
 
-const web3 = new Web3(`ws://127.0.0.1:8545`)
+// const web3 = new Web3(`ws://127.0.0.1:8545`)
 
-const myContract = new web3.eth.Contract(config.abi,config.contractAddress);
+// const myContract = new web3.eth.Contract(config.abi,config.contractAddress);
 
 let options = {
     filter:{
@@ -86,33 +86,33 @@ let options = {
     fromBlock: 'latest'
 };
 
-myContract.events.Minted(options).on('data', event=> {
-    console.log(event.returnValues.to);
+// myContract.events.Minted(options).on('data', event=> {
+//     console.log(event.returnValues.to);
     
-    User.findOne({publicAddr:event.returnValues.to.toLowerCase()})
-    .then((user)=>{
-        if(user){
-            user.ownerOfNFT[0].NFT_URL.push(event.returnValues.tokenURI);
-            console.log(user.ownerOfNFT[0].NFT_URL);
-            user.save();
-        }else{
-            const user1 = new User({
-                publicAddr: event.returnValues.to.toLowerCase(),
-                ownerOfNFT:[{collection_id:"NCC 1st", 
-                    NFT_URL:[event.returnValues.tokenURI]}],
-                role:"user",
-                profile:{username:event.returnValues.to.toLowerCase(), 
-                caption:"", 
-                points: 0,         
-                post_ids:[], 
-                profile_pic:event.returnValues.tokenURI
-            }})
-            console.log(user1)
-            user1.save();
-        }
+//     User.findOne({publicAddr:event.returnValues.to.toLowerCase()})
+//     .then((user)=>{
+//         if(user){
+//             user.ownerOfNFT[0].NFT_URL.push(event.returnValues.tokenURI);
+//             console.log(user.ownerOfNFT[0].NFT_URL);
+//             user.save();
+//         }else{
+//             const user1 = new User({
+//                 publicAddr: event.returnValues.to.toLowerCase(),
+//                 ownerOfNFT:[{collection_id:"NCC 1st", 
+//                     NFT_URL:[event.returnValues.tokenURI]}],
+//                 role:"user",
+//                 profile:{username:"안녕하세요", 
+//                 caption:"", 
+//                 points: 0,         
+//                 post_ids:[], 
+//                 profile_pic:event.returnValues.tokenURI
+//             }})
+//             console.log(user1)
+//             user1.save();
+//         }
         
-    })
-});
+//     })
+// });
 
 // authentication을 위한 함수
 // io.use((socket, next)=>{
