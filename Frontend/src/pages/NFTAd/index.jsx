00aa61cf9de46d2635d4f7f9789da7a1ef92254d
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2022-12-06 16:00:22 +0900

    feat: 광고판 설정 이미지 preview 추가

--- Frontend/src/pages/NFTAd/index.jsx
+++ Frontend/src/pages/NFTAd/index.jsx
@@ -15,22 +15,23 @@ export const NFTAd = () => {
 
   const onChangeImage = (event) => {
     const reader = new FileReader();
-    const canvas = document.createElement('canvas');
-    const context = canvas.getContext("2d");    
+    const canvas = document.getElementById("tcanvas");
+    const context = canvas.getContext("2d");
 
     reader.onload = (event) => {
       const img = new Image();
       img.onload = () => {
-        context.drawImage(img, 0, 0);
+        context.drawImage(img, 0, 0, 10 , 10);
         let pixelData = [];
         const contextPixel = context.getImageData(0, 0 , 10, 10);
-        console.log(contextPixel);
+        
         for(let i = 0; i < contextPixel.data.length; i+=4){
           pixelData.push(contextPixel.data[i]);
           pixelData.push(contextPixel.data[i+1]);
           pixelData.push(contextPixel.data[i+2]);
         }        
         setRgbData(pixelData);
+        context.drawImage(img, 0, 0, 100, 100);
       }
       img.src = event.target.result;            
     }
@@ -75,6 +76,10 @@ export const NFTAd = () => {
             name=""
             id="input-image"
           />
+        </div>
+        <div className="NFTAd-content">
+          <p className="NFTAd-content-title">미리보기</p>
+          <canvas id="tcanvas" width="100px" height="100px"/>
         </div>
         <div className="NFTAd-content">
           <p className="NFTAd-content-title">링크 주소</p>
--- backend/services/billboard/routes.js
+++ backend/services/billboard/routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const controller = require('./controller');
+const verify = require('../../middleware/jwt');
 
 const billboardRouter = express.Router();
 
-billboardRouter.route('/').post(controller.personalize);
+billboardRouter.route('/').post(verify, controller.personalize);
 
 module.exports = billboardRouter;
