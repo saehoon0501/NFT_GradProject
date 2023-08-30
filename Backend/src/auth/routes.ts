ab56e30b47ee0acb3c746a98deb4d0d1d559dd6c
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-08-30 19:55:40 +0900

    auth service refactoring

--- Backend/src/auth/routes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import controller from "./controller";
-
-const authRouter = express.Router();
-
-authRouter.route("/").post(controller.sndJwt);
-authRouter.route("/").get(controller.sndNonce);
-
-export = authRouter;
--- Backend/src/routes.ts
+++ Backend/src/routes.ts
@@ -2,17 +2,13 @@ import express from "express";
 import postRouter from "./posts";
 import pollRouter from "./polls";
 import uploadRouter from "./uploads";
-// import billboardRouter from "./components/billboard";
 import { verify } from "./middleware/jwt";
 
 const router = express.Router();
 
-// router.use("/auth", authRouter);
 router.use(verify);
-// router.use("/user", userRouter);
 router.use("/post", postRouter);
 router.use("/poll", pollRouter);
 router.use("/upload", uploadRouter);
-// router.use("/billboard", billboardRouter);
 
 export { router };
