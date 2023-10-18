69e197b69b05a464e9d6476212b76f552325efdc
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2023-10-19 08:26:08 +0900

    Update ProfilePic.jsx

--- Frontend/src/components/profile/ProfilePic.jsx
+++ Frontend/src/components/profile/ProfilePic.jsx
@@ -13,12 +13,26 @@ export const ProfilePic = ({ user, isOwner = true }) => {
     <>
       {isOwner ? (
         <div className="profile-info" onClick={handlePic}>
-          <img className="profile-info-img" src={user.profile_pic} />
+          <img
+            className="profile-info-img"
+            src={
+              user.profile_pic === " "
+                ? "https://as2.ftcdn.net/v2/jpg/03/49/49/79/1000_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg"
+                : user.profile_pic
+            }
+          />
           <p className="profile-edit-img">이미지 수정</p>
         </div>
       ) : (
         <div className="profile-info-not-owner">
-          <img className="profile-info-img" src={user.profile_pic} />
+          <img
+            className="profile-info-img"
+            src={
+              user.profile_pic === " "
+                ? "https://as2.ftcdn.net/v2/jpg/03/49/49/79/1000_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg"
+                : user.profile_pic
+            }
+          />
           <p className="profile-edit-img">이미지 수정</p>
         </div>
       )}
